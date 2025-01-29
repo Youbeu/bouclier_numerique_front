d0b5b5d805d25e5de67645487cb8a60584289c33
Christian YAC <christianyoubeu@gmail.com>
2025-01-29 12:22:21 +0100

    Mise à jour technique
    - Installation et configuration du package Axios pour consommer les API provenant de notre backend.
    - Gestion de l'authentification ( Inscription et Connexion)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,60 @@
-import { Link } from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axiosInstance from '../api/axiosInstance';
 
 const Login = () => {
-    return ( 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === 'email') setEmail(value);
+        if (name === 'password') setPassword(value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        try {
+            const response = await axiosInstance.post("utilisateur/connexion/", { email, password });
+            console.log("Connexion réussie :", response.data);
+            localStorage.setItem('access_token', response.data.access);
+            localStorage.setItem('refresh_token', response.data.refresh);
+            setIsLoggedIn(true);
+            navigate('/dashboard');
+        } catch (error) {
+            const errorMessage = error.response?.data?.detail || "Identifiants incorrects.";
+            console.error("Erreur lors de la connexion", error.response?.data);
+            setError(errorMessage);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return (
         <div className="sign-up">
             <h2>Accédez à Votre Forteresse Numérique</h2>
-            <p>Entrez vos identifiants pour déverrouiller votre espace sécurisé. Chaque connexion renforce votre contrôle dans la guerre numérique.</p>
-            <form>
+            <p>Entrez vos identifiants pour déverrouiller votre espace sécurisé.</p>
+            <form onSubmit={handleSubmit}>
                 <label>Adresse e-mail</label>
-                <input type="email" required />
+                <input name='email' value={email} onChange={handleChange} type="email" required />
                 <label>Mot de passe</label>
-                <input type="password" required />
-                <button style={{width: "100%", color:'#f1356d', marginTop:'20px'}}>Déverrouiller Mon Accès</button>
-                <div style={{display: "flex", justifyContent: "space-around", marginTop:"20px"}}>
+                <input name='password' value={password} onChange={handleChange} type="password" required />
+                <p style={{ color: 'red' }}>{error}</p>
+                <button type="submit" style={{ width: "100%", color: '#f1356d', marginTop: '20px' }} disabled={isLoading}>
+                    {isLoading ? "Connexion en cours..." : "Déverrouiller Mon Accès"}
+                </button>
+                <div style={{ display: "flex", justifyContent: "space-around", marginTop: "20px" }}>
                     <p>Vous n'avez pas de compte ?</p>
                     <Link to="/sign-up">Inscrivez-vous</Link>
                 </div>
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Login;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,20 +1,87 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axiosInstance from "../api/axiosInstance";
 
 const SignUp = () => {
+
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    // Fonction pour générer un mot de passe sécurisé
+    const generatePassword = () => {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+        const passwordLength = 10;
+        let password = '';
+        for (let i = 0; i < passwordLength; i++) {
+            const randomIndex = Math.floor(Math.random() * chars.length);
+            password += chars[randomIndex];
+        }
+        setFormData({ ...formData, password });
+    }; 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axiosInstance.post("utilisateur/inscription/", formData);
+            console.log("Inscription réussie :", response.data);
+            navigate('/dashboard'); // si ca marche bah redirection vers le dashboard
+        } catch (error) {
+            if (error.response && error.response.data) {
+                console.error("Erreur lors de l'inscription", error.response.data);
+                setError("Échec de l'inscription : " + JSON.stringify(error.response.data));
+            } else {
+                console.error("Erreur inattendue", error);
+                setError("Une erreur inattendue s'est produite. Veuillez réessayer.");
+            }
+        }
+    };
+    
+    
+
     return ( 
         <div className="sign-up">
             <h2>Un Compte, Un Bouclier</h2>
             <p>Votre première ligne de défense commence ici. Créez un compte et prenez les commandes de votre sécurité numérique.</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Nom Complet</label>
-                <input type="text" required />
+                <input 
+                    type="text" 
+                    name="username"
+                    required  
+                    onChange={handleChange}
+                    value={formData.username}
+                />
                 <label>Adresse e-mail</label>
-                <input type="email" required />
+                <input 
+                    type="email" 
+                    name="email"
+                    required 
+                    onChange={handleChange}
+                    value={formData.email}
+                />
                 <label>Mot de passe</label>
-                <div style={{display: "flex", justifyContent: "space-between", marginBottom:"30px"}}>
-                    <input type="password" required />
-                    <button style={{color:'#f1356d'}}>Générer</button>
+                <div style={{display: "flex", justifyContent: "space-between", marginBottom:"30px", alignItems: "center"}}>
+                    <input 
+                        type="password" 
+                        name="password"
+                        required 
+                        style={{flex: 1, marginRight: "10px"}}  
+                        onChange={handleChange}
+                        value={formData.password}
+                    />
+                    <button style={{color:'#f1356d'}} type="button" onClick={generatePassword}>Générer</button>
                 </div>
+                <p style={{color: "red"}}>{error}</p>
                 <button style={{width: "100%", color:'#f1356d'}}>Créer un compte</button>
                 <div style={{display: "flex", justifyContent: "space-around", marginTop:"20px"}}>
                     <p>Vous avez déjà un compte ?</p>
